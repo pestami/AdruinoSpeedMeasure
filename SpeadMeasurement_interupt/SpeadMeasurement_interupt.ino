65b15851c96aaa7ecbbca20702e19617769e889a
Mirek Pesta <mirekpesta@gmail.com>
2025-03-07 19:50:24 +0100

    ....

--- SpeadMeasurement_interupt/SpeadMeasurement_interupt.ino
+++ SpeadMeasurement_interupt/SpeadMeasurement_interupt.ino
@@ -17,13 +17,23 @@ unsigned long elapsedTime;
 
 unsigned long interuptStartTime;
 unsigned long interuptStopTime;
-unsigned long speed;
+long speed;
 
 const int Button_7 = 7; 
 int Button_State_7 = 0;
+int Button_Start = 0;  // variable for reading the pushbutton status
 
 const int Button_1 = 1; 
 int Button_State_1 = 0;
+int Button_Stop = 0; 
+
+int FlagMeasurementDone=0;
+int FlagMeasurementStart=0;
+int FlagMeasurementStop=0;
+int measurementCounter = 0; 
+
+String sSpeed="";
+String sSpeedUnformated="";
 //========================================================================================
 void setup()
 {
@@ -32,8 +42,8 @@ void setup()
   pinMode(Button_7, INPUT);             // initialize t:  
   pinMode(Button_1, INPUT);             // initialize t:  
 
-   attachInterrupt(digitalPinToInterrupt(Button_7), measurestart, CHANGE);
-   attachInterrupt(digitalPinToInterrupt(Button_1), measurestop, CHANGE);
+   attachInterrupt(digitalPinToInterrupt(Button_7), measurestart, RISING);
+   attachInterrupt(digitalPinToInterrupt(Button_1), measurestop, FALLING);
 
                                             
   Serial.begin(9600);  //Initiate Serial communication.
@@ -42,32 +52,37 @@ void setup()
    void measurestart() {
 
       interuptStartTime= millis();
+      FlagMeasurementStop=0;
+      FlagMeasurementStart=1;
 
     }
 
    void measurestop() {
 
-      interuptStopTime= millis();
+      interuptStopTime= millis();  //millis()   micros()
+      FlagMeasurementStop=1;
+      FlagMeasurementStart=0;
 
     }
 
 //========================================================================================
 void loop()
 {
-   Serial.print("\n========SPEAD============\n");
-  
-     
-      Serial.print("\n-interuptstartTime: \n");
-      Serial.print(interuptStartTime);
-      Serial.print("\n-interupStopTime: \n");
-      Serial.print(interuptStopTime);
 
-      if  (interuptStopTime > interuptStartTime)
+
+    Button_Start = digitalRead(Button_7);
+    Button_Stop = digitalRead(Button_1);
+
+
+
+
+
+     // if  (interuptStopTime > interuptStartTime)
+      if (FlagMeasurementStart==0 && FlagMeasurementStop==1)
       {
-        elapsedTime=interuptStopTime-interuptStartTime;
-        Serial.print("\nELASPEED TIME: \n");
-        Serial.print(elapsedTime);
 
+        elapsedTime=interuptStopTime-interuptStartTime;
+        FlagMeasurementDone=1;
         lcd.setCursor(0, 0);         // move cursor to   (2, 1)   
         lcd.print("Elapsed Time:");    
       
@@ -76,21 +91,65 @@ void loop()
         lcd.setCursor(0, 1);         // move cursor to   (2, 1)  
         lcd.print(elapsedTime);   
 
-        lcd.setCursor(5, 1);         // move cursor to   (2, 1)  
+        lcd.setCursor(7, 1);         // move cursor to   (2, 1)  
         lcd.print("ms");   
 
-        lcd.setCursor(8, 1);         // move cursor to   (2, 1)  
-        speed=85000;
-        speed=speed/elapsedTime;
-        speed=speed/1000;
+                 // move cursor to   (2, 1)  
+        speed=85*1000*10;   //*1000; // mm
+        speed=speed/elapsedTime; // micro second
+        speed=speed;
+        //speed=123;
+        sSpeed=String(speed);
+        sSpeedUnformated=sSpeed;
+        sSpeed=sSpeed.substring(0,1)+"."+ sSpeed.substring(1,1);
 
-        lcd.print(speed); 
+        lcd.setCursor(10, 1);
+        lcd.print(sSpeed); 
         lcd.setCursor(13, 1);         // move cursor to   (2, 1) 
         lcd.print("m/s"); 
-      }
-     
 
- delay(200);               // display the above for two seconds
+          measurementCounter+=1;
+
+            Serial.print("\n========SPEED============");
+            
+            Serial.print("\nSCounter: " + String(measurementCounter) + "");
+            Serial.print("\nStart Button:" + String(Button_Start) + "");
+            Serial.print("\nStop Button:" + String(Button_Stop) + "");     
+            Serial.print("\nStart Interupt:" + String(FlagMeasurementStart) + "");
+            Serial.print("\nStop Interupt:" + String(FlagMeasurementStop) + "");     
+            Serial.print("\n-interuptstartTime: ");
+            Serial.print(interuptStartTime);
+            Serial.print("\n-interupStopTime: ");
+            Serial.print(interuptStopTime);
+            Serial.print("\nELASPEED TIME: ");
+            Serial.print(elapsedTime);
+            Serial.print("\n-Speed String: ");
+           Serial.print(":" + sSpeedUnformated+ ":");
+            Serial.print("\n-Speed String len: ");
+           Serial.print(sSpeedUnformated.length());
+            Serial.print("\n=========================\n");
+
+      FlagMeasurementStop=0;
+      FlagMeasurementStart=0;
+      }
+
+  if (Button_Start==LOW && Button_Stop==HIGH)
+    {
+      interuptStartTime=0;
+      interuptStopTime=0;
+        lcd.setCursor(0, 0);         // move cursor to   (2, 1)   
+        lcd.print("Device Ready");   
+        //lcd.setCursor(0, 1);         // move cursor to   (2, 1)   
+        //lcd.print("----------------");   
+        // FlagMeasurementDone=0;
+        // FlagMeasurementStop=0;
+        //FlagMeasurementStart=1;
+            // Serial.print("\n========SPEED============");
+            // Serial.print("\nReady to go !!! ");
+            // Serial.print("\n=========================\n");
+    }
+
+ delay(2000);               // display the above for two seconds
 
 }
 //========================================================================================
