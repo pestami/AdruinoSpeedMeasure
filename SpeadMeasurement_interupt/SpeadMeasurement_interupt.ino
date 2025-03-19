9db1dda7e7fe0a6c97c4421d129f3d36ea7c8ee6
Mirek Pesta <mirekpesta@gmail.com>
2025-03-19 17:23:52 +0100

    State machine v1

--- SpeadMeasurement_interupt/SpeadMeasurement_interupt.ino
+++ SpeadMeasurement_interupt/SpeadMeasurement_interupt.ino
@@ -8,6 +8,8 @@ Micro, Leonardo	0, 1, 2, 3, 7
 */
 
 //========================================================================================
+// DECLARATIONS
+//========================================================================================
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
@@ -17,7 +19,7 @@ unsigned long elapsedTime;
 
 unsigned long interuptStartTime;
 unsigned long interuptStopTime;
-long speed;
+long int speed;
 
 const int Button_7 = 7; 
 int Button_State_7 = 0;
@@ -34,117 +36,178 @@ int measurementCounter = 0;
 
 String sSpeed="";
 String sSpeedUnformated="";
+int intLen=0;
+
+int MStatePre=1111;
+int MState=1111;
 //========================================================================================
-void setup()
+// INTERUPT HANDLERS
+//========================================================================================
+      #include <TimerOne.h>
+      int Timer_Loop=0; 
+    void Timer1_ISR(void)
+      {
+        Timer_Loop=Timer_Loop + 1;
+        if (Timer_Loop==11){ Timer_Loop=0;}   // timer counts one to 10
+      }   
+   void measurestart() 
+      {
+        interuptStartTime= millis();
+        MStatePre=MState;
+        MState=101;
+        FlagMeasurementStop=0;
+        FlagMeasurementStart=1;
+      }
+   void measurestop() 
+      {
+        if (FlagMeasurementStart==1)
+        {
+          interuptStopTime= millis();  //millis()   micros()
+          MStatePre=MState;
+          MState=110;
+          FlagMeasurementStop=1;
+          FlagMeasurementStart=0;
+        }
+      }
+//========================================================================================
+// PROCEDURES
+//========================================================================================
+void DebugPrint()
 {
-  lcd.init(); // initialize the lcd
-  lcd.backlight();  
-  pinMode(Button_7, INPUT);             // initialize t:  
-  pinMode(Button_1, INPUT);             // initialize t:  
+  Serial.print("\n=======MACHINE STATE=====");
+  
+  Serial.print("\nSCounter: " + String(measurementCounter) + "");
+  Serial.print("\nStart Button:" + String(Button_Start) + "");
+  Serial.print("\nStop Button:" + String(Button_Stop) + "");     
+  Serial.print("\nStart Interupt:" + String(FlagMeasurementStart) + "");
+  Serial.print("\nStop Interupt:" + String(FlagMeasurementStop) + "");  
 
-   attachInterrupt(digitalPinToInterrupt(Button_7), measurestart, RISING);
-   attachInterrupt(digitalPinToInterrupt(Button_1), measurestop, FALLING);
+  Serial.print("\n MachineState_now =:" + String(MState) + "");  
+  Serial.print("\n nMachineState_Pre = :" + String(MStatePre) + "");  
 
-                                            
-  Serial.begin(9600);  //Initiate Serial communication.
+  Serial.print("\n========SPEED============");   
+
+  Serial.print("\n-interuptstartTime: ");
+  Serial.print(interuptStartTime);
+  Serial.print("\n-interupStopTime: ");
+  Serial.print(interuptStopTime);
+  Serial.print("\nELASPEED TIME: ");
+  Serial.print(elapsedTime);
+  Serial.print("\n-Speed String: ");
+  Serial.print(":" + sSpeedUnformated+ ":");
+  Serial.print("\n-Speed String len: ");
+  Serial.print(sSpeedUnformated.length());
+  
+  Serial.print("\n Speed=" + sSpeed + "");    
+  Serial.print("\n=========================\n");
 
 }
-   void measurestart() {
+//========================================================================================
+// SETUP
+//========================================================================================
+void setup()
+        {
+          lcd.init(); // initialize the lcd
+          lcd.backlight();  
+          pinMode(Button_7, INPUT);             // initialize t:  
+          pinMode(Button_1, INPUT);             // initialize t:  
 
-      interuptStartTime= millis();
-      FlagMeasurementStop=0;
-      FlagMeasurementStart=1;
-
-    }
-
-   void measurestop() {
-
-      interuptStopTime= millis();  //millis()   micros()
-      FlagMeasurementStop=1;
-      FlagMeasurementStart=0;
-
-    }
-
+          attachInterrupt(digitalPinToInterrupt(Button_7), measurestart, RISING);
+          attachInterrupt(digitalPinToInterrupt(Button_1), measurestop, FALLING);
+          
+            Timer1.initialize(100*1000); // Fire An Interrupt Every 100ms // Timer_Loop counts to 10 = 1 count per second
+            Timer1.attachInterrupt(Timer1_ISR);
+                                                    
+          Serial.begin(9600);  //Initiate Serial communication.
+        }
+//========================================================================================
+//MAIN LOOP
 //========================================================================================
 void loop()
-{
-    Button_Start = digitalRead(Button_7);
-    Button_Stop = digitalRead(Button_1);
+{   if (Timer_Loop==0)   // every second buttons are read
+      {
+        Button_Start = digitalRead(Button_7);
+        Button_Stop = digitalRead(Button_1);
+      }    
+    if (Button_Start==LOW && Button_Stop==HIGH)  //
+      {
+         MStatePre=101;
+         MState=101;   // Device Ready Pre state reset
+
+        interuptStartTime=0;
+        interuptStopTime=0;
+
+          if ((Timer_Loop % 3) ==0 )   // every 10/3 second buttons are read
+            {
+                lcd.setCursor(0, 0);         // move cursor to   (2, 1)   
+                lcd.print("Device Ready    ");   
+                lcd.setCursor(0, 1);         // move cursor to   (2, 1)   
+                lcd.print("----------------");  
+            }
+
+          // delay(2000); 
+      }
 
      // if  (interuptStopTime > interuptStartTime)
       if (FlagMeasurementStart==0 && FlagMeasurementStop==1)
       {
-
         elapsedTime=interuptStopTime-interuptStartTime;
         FlagMeasurementDone=1;
-        lcd.setCursor(0, 0);         // move cursor to   (2, 1)   
-        lcd.print("Elapsed Time:");    
+        //lcd.setCursor(0, 0);         // move cursor to   (2, 1)   
+        //lcd.print("Elapsed Time:");    
       
         lcd.setCursor(0, 1);         // move cursor to   (2, 1)  
         lcd.print("           ");     
-        lcd.setCursor(0, 1);         // move cursor to   (2, 1)  
-        lcd.print(elapsedTime);   
+        //lcd.setCursor(0, 1);         // move cursor to   (2, 1)  
+        //lcd.print(elapsedTime);   
 
        // lcd.setCursor(7, 1);         // move cursor to   (2, 1)  
        // lcd.print("ms");   
 
                  // move cursor to   (2, 1)  
-        speed=85*1000*10;   //*1000; // mm
-        speed=speed/elapsedTime; // micro second
-        speed=speed;
-        //speed=123;
-        sSpeed=String(speed);
-        sSpeedUnformated=sSpeed;
-        sSpeed=sSpeed.substring(0,3)+"."+ sSpeed.substring(3,0);  //string substr (size_t pos, size_t len) const;
-        //sSpeed=sSpeedUnformated;
+        speed=85*1000*10;   //*1000*10 to avoid truncation of digits; // mm
+        speed=speed/elapsedTime; // time is micro second
 
-        lcd.setCursor(7, 1);
-        lcd.print(sSpeed); 
-        lcd.setCursor(13, 1);         // move cursor to   (2, 1) 
-        lcd.print("m/s"); 
+         
+        sSpeed=String(speed);
+        sSpeedUnformated=sSpeed; // variable for debug
+        intLen=sSpeedUnformated.length();
+        sSpeed=sSpeed.substring(0,3)+"."+ sSpeed.substring(3,0);  //string substr (size_t pos, size_t len) const;
+
+        speed=speed/1000;
+        sSpeedUnformated=String(speed);
+
+        //long int v = 123456; 
+        ///printf("%.1f\n", v / 10000.0);
+
+	      //char output[10];
+	      //sprintf(output, "%.1f m/s", speed/10.0/1000.0); //speed = long int
+        //sSpeed=String(output);
+
+        lcd.setCursor(0, 0);         // move cursor to   (2, 1)   
+        lcd.print("              ");    
+        lcd.setCursor(0, 0);         // move cursor to   (2, 1)   
+        lcd.print("Measurement Done");  
+        lcd.setCursor(0, 1);         // move cursor to   (2, 1)   
+        lcd.print("                ");   
+        lcd.setCursor(0, 1);
+        //lcd.print(sSpeed); 
+        lcd.print(sSpeedUnformated.substring(0,14));
+        //lcd.setCursor(13, 1);         // move cursor to   (2, 1) 
+        //lcd.print("m/s"); 
 
           measurementCounter+=1;
 
-            Serial.print("\n========SPEED============");
-            
-            Serial.print("\nSCounter: " + String(measurementCounter) + "");
-            Serial.print("\nStart Button:" + String(Button_Start) + "");
-            Serial.print("\nStop Button:" + String(Button_Stop) + "");     
-            Serial.print("\nStart Interupt:" + String(FlagMeasurementStart) + "");
-            Serial.print("\nStop Interupt:" + String(FlagMeasurementStop) + "");     
-            Serial.print("\n-interuptstartTime: ");
-            Serial.print(interuptStartTime);
-            Serial.print("\n-interupStopTime: ");
-            Serial.print(interuptStopTime);
-            Serial.print("\nELASPEED TIME: ");
-            Serial.print(elapsedTime);
-            Serial.print("\n-Speed String: ");
-           Serial.print(":" + sSpeedUnformated+ ":");
-            Serial.print("\n-Speed String len: ");
-           Serial.print(sSpeedUnformated.length());
-            Serial.print("\n=========================\n");
+           DebugPrint()
 
       FlagMeasurementStop=0;
       FlagMeasurementStart=0;
+         delay(4000);               // display the above for two seconds
       }
 
-  if (Button_Start==LOW && Button_Stop==HIGH)
-    {
-      interuptStartTime=0;
-      interuptStopTime=0;
-        lcd.setCursor(0, 0);         // move cursor to   (2, 1)   
-        lcd.print("Device Ready");   
-        //lcd.setCursor(0, 1);         // move cursor to   (2, 1)   
-        //lcd.print("----------------");   
-        // FlagMeasurementDone=0;
-        // FlagMeasurementStop=0;
-        //FlagMeasurementStart=1;
-            // Serial.print("\n========SPEED============");
-            // Serial.print("\nReady to go !!! ");
-            // Serial.print("\n=========================\n");
-    }
 
- delay(2000);               // display the above for two seconds
+
+ //delay(2000);               // display the above for two seconds
 
 }
 //========================================================================================
